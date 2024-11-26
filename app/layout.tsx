5459314ca9d373f1ab2cd9fb36af7ca54db89dc3
pawanshettyy <pawanshetty13092006@gmail.com>
2024-11-26 21:30:18 +0530

    Fixed Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "StoreIt by Pawan",
+  title: "StoreIt",
   description: "StoreIt - The only storage solution you need.",
 };
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Search from "./Search";
-import FileUploader from "./FileUploader";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
-const Header = () => {
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
-        <FileUploader ownerId="yourOwnerId" accountId="yourAccountId" />
-        <form>
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}
+        >
           <Button type="submit" className="sign-out-button">
             <Image
               src="/assets/icons/logout.svg"
