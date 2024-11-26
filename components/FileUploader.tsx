5fd2105dcf6615cc8ddcc726ffcb40e0971f5123
pawanshettyy <pawanshetty13092006@gmail.com>
2024-11-26 20:46:51 +0530

    Fixed Props error

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const FileUploader = () => {
+interface FileUploaderProps {
+  ownerId: string;
+  accountId: string;
+}
+
+const FileUploader: React.FC<FileUploaderProps> = ({ ownerId, accountId }) => {
   return (
-    <div>FileUploader</div>
+    <div>
+      FileUploader
+      <p>Owner ID: {ownerId}</p>
+      <p>Account ID: {accountId}</p>
+    </div>
   )
 }
 
